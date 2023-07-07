16220f1bc96bede5384066710f915d2bacf36009
nandayanam <108791734+nandayanam@users.noreply.github.com>
2023-07-07 23:52:26 +0530

    Add files via upload

---
+++ etl.py
@@ -0,0 +1,39 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect(
+        host=config['CLUSTER']['HOST'],
+        dbname=config['CLUSTER']['DB_NAME'],
+        user=config['CLUSTER']['DB_USER'],
+        password=config['CLUSTER']['DB_PASSWORD'],
+        port=config['CLUSTER']['DB_PORT'],
+        iam_role=config['IAM_ROLE']['ARN']
+    )
+    cur = conn.cursor()
+    
+    load_staging_tables(cur, conn)
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
